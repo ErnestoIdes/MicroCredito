1953f397a481279f7f66ba9bec019e374581fa9a
Ernesto Ides Rafael Simango <30523874+ErnestoIdes@users.noreply.github.com>
2021-01-12 10:56:52 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+
+version: 2
+
+ 
+
+    steps:
+      - put the project at server folder ´www´ or ´htdocs´
+      - import the data base file located at the root of the project, named "micro.sql"
+      - start the server
+      - open CMD browse to the project folder
+      
+      - serve the project
+
+      - credentials commands:
+            email: neto.sima@gmail.com
+            pass: 12345678
+      
